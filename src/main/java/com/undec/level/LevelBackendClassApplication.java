f947f548da938fca048d2e845aeb280a05535002
hoel <hoelcross18@gmail.com>
2021-09-01 12:01:46 -0300

    Proyecto base

---
+++ src/main/java/com/undec/level/LevelBackendClassApplication.java
@@ -0,0 +1,13 @@
+package com.undec.level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelBackendClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelBackendClassApplication.class, args);
+    }
+
+}
